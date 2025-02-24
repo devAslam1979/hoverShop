a2ebaa63e47bfc0da8591049e115b53692159f2d
Md Aslam <dev.aslam1979@gmail.com>
2025-02-24 11:49:20 +0530

    added access and refresh token in auth

--- backend/src/controllers/users/authController.ts
+++ backend/src/controllers/users/authController.ts
@@ -1,24 +1,22 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import User from "../../models/User";
-
+import jwt, { JwtPayload } from "jsonwebtoken";
 export const register = async (req: Request, res: Response) => {
   try {
     const { name, email, phone, password } = req.body;
     if (!name || !email || !phone || !password) {
-      res.status(400).json({
+      return res.status(400).json({
         status: false,
         message: "All fields are required",
       });
-      return;
     }
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      res.status(200).json({
+      return res.status(200).json({
         success: false,
         message: "User already exists",
       });
-      return;
     }
     const passwordHash = await bcrypt.hash(password, 10);
     const newUser = new User({
@@ -28,9 +26,27 @@ export const register = async (req: Request, res: Response) => {
       password: passwordHash,
     });
     await newUser.save();
+    const accessToken = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_ACCESS_SECRET as string,
+      {
+        expiresIn: "15m",
+      }
+    );
+    const refreshToken = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_REFRESH_SECRET as string,
+      {
+        expiresIn: "7d",
+      }
+    );
     res.status(201).json({
       success: true,
       message: "User registered successfully",
+      data: {
+        accessToken,
+        refreshToken,
+      },
     });
   } catch (error) {
     console.log(error);
@@ -45,33 +61,44 @@ export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      res.status(400).json({
+      return res.status(400).json({
         success: false,
         message: "All fields are required",
       });
-      return;
     }
     const user = await User.findOne({ email });
     if (!user) {
-      res.status(404).json({
+      return res.status(404).json({
         status: false,
         message: "User not found",
       });
-      return;
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      res.status(401).json({
+      return res.status(401).json({
         success: false,
         message: "Invalid credentials",
       });
-      return;
     }
-    const { password: _, ...userWithoutPassword } = user.toObject();
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_ACCESS_SECRET as string,
+      { expiresIn: "15m" }
+    );
+    const refreshToken = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_REFRESH_SECRET as string,
+      {
+        expiresIn: "7d",
+      }
+    );
     res.status(200).json({
       success: true,
       message: "User logged in successfully",
-      user: userWithoutPassword,
+      data: {
+        accessToken,
+        refreshToken,
+      },
     });
   } catch (error) {
     console.log(error);
@@ -84,9 +111,42 @@ export const login = async (req: Request, res: Response) => {
 
 export const refresh = async (req: Request, res: Response) => {
   try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token is required",
+      });
+    }
+    const decoded = jwt.verify(
+      refreshToken,
+      process.env.JWT_REFRESH_SECRET as string
+    ) as JwtPayload;
+    if (!decoded || typeof decoded === "string") {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid refresh token",
+      });
+    }
+    const accessToken = jwt.sign(
+      { userId: decoded.userId },
+      process.env.JWT_ACCESS_SECRET as string,
+      { expiresIn: "15m" }
+    );
+    const newRefreshToken = jwt.sign(
+      { userId: decoded.userId },
+      process.env.JWT_REFRESH_SECRET as string,
+      {
+        expiresIn: "7d",
+      }
+    );
     res.status(200).json({
       success: true,
-      message: "User refreshed successfully",
+      message: "Access token refreshed successfully",
+      data: {
+        accessToken,
+        refreshToken: newRefreshToken,
+      },
     });
   } catch (error) {
     console.log(error);
